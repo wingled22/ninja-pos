5fed6fd704418bc6e805a07e66e583d73d99d8ac
oninsan <kokoybaldofordawin@gmail.com>
2025-02-19 22:10:28 +0800

    Done setting up barebone redux

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { store } from "./utils/store.ts";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router";
+import AppRouter from "./AppRouter.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <Router>
+        <AppRouter />
+      </Router>
+    </Provider>
+  </StrictMode>
+);

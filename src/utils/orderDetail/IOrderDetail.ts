286e37d239851b7d697f662f779b97d2ca164a2a
rikimaruu333 <crmchs.monsales.ricky@gmail.com>
2025-02-26 20:12:46 +0800

    Get All Order Detail function

---
+++ src/utils/orderDetail/IOrderDetail.ts
@@ -0,0 +1,9 @@
+interface OrderDetail {
+    orderDetailId: number;
+    orderId: number;
+    productSkuId: number;
+    orderQuantity: number;
+    orderTotalPrice: number;
+  }
+  export default OrderDetail;
+  
